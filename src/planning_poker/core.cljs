10afd345dd4894ff06dc3ef953c3de69f13fb4bb
John Flinchbaugh <john@hjsoft.com>
2020-03-14 17:57:27 -0400

    planning poker implementation ported from jquery

--- src/planning_poker/core.cljs
+++ src/planning_poker/core.cljs
@@ -3,11 +3,32 @@
       [reagent.core :as r]
       [reagent.dom :as d]))
 
+;; --------
+;; State
+
+(defonce selected (r/atom nil))
+
+(def card-values (map str [0 (char 0x00BD) 1 2 3 5 8 13 20 40 100 \? (char 0x221E)]))
+
+(defn select-card! [v]
+  (reset! selected v))
+
 ;; -------------------------
 ;; Views
 
+(defn card [v]
+  [:div [:div.big.card {:on-click #(select-card! nil)}[:span v]]])
+
+(defn cards [vs]
+  [:div.grid
+   (for [v vs]
+     ^{:key v}
+     [:div.small.card {:on-click #(select-card! v)} v])])
+
 (defn home-page []
-  [:div [:h2 "Welcome to Reagent"]])
+  (if @selected
+    (card (str @selected))
+    (cards card-values)))
 
 ;; -------------------------
 ;; Initialize app
